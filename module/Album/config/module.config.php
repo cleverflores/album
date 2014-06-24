b2167b5b5b61924dde5bc28ddcbd3d705d319d84
Administrador <Administrador@pepito-bc5fe5c9>
2014-06-24 21:24:48 +0200

    Creado el modulo Album, controlador album

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -12,9 +12,8 @@ return array(
             'album' => array(
                 'type' => 'segment',
                 'options' => array(
-                    'route' => '/album[/:controller[/:action]][/:id]',
-                    'constraints' => array(
-                       'controller' => '[a-zA-Z][a-zA-Z0-9_-]*', 
+                    'route' => '/album[/:action][/:id]',
+                    'constraints' => array(                       
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                         'id' => '[0-9]+',
                     ),
